9cb0a9b5eab0ddbf4b0e6fb1866588cd4bc70a22
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-16 16:39:36 +0100

    layout toDoList

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        toDoList: [
+            {
+                text: "spesa",
+                done: "true",
+            },
+            {
+                text: "compiti",
+                done: "false",
+            },
+            {
+                text: "pulizie",
+                done: "false",
+            },
+            {
+                text: "cucinare",
+                done: "true",
+            },
+        ]
+      }
+    }
+  }).mount('#app')
